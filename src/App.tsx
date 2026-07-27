d2954103a33198ee9bd746abdd1934b4e5986476
Jonell Balanay <itsjonellmb@gmail.com>
2026-07-27 16:20:55 +1200

    tailwind-test-refresher

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 function App() {
   return (
     <div>
-      <h1>Weather Dashboard</h1>
+      <div className="bg-violet-200 h-10 w-full border-2 border-violet-600 rounded-md my-4 p-2 flex justify-center items-center">
+        <h1 className="text-center font-mono font-extrabold text-[30px]">Weather Dashboard</h1>
+      </div>
+      <div className="grid grid-cols-3 gap-2 mt-2 mx-2">
+        <div className="h-16 rounded-full bg-blue-500"></div>
+        <div className="h-16 rounded-full bg-orange-500"></div>
+        <div className="h-16 rounded-full bg-green-500"></div>
+      </div>
+      <div className="sm:bg-amber-500 md:bg-amber-700">show up please</div>
     </div>
   );
 }
